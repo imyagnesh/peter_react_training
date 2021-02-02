dd58ba60a094c58ebaffa46dd01bd460139378fe
imyagnesh <yagnesh.modh@gmail.com>
2021-02-02 09:32:16 +0530

    generarot, classes, lops

---
+++ Promises.js
@@ -0,0 +1,68 @@
+// race
+// all
+// any
+// allsettled
+
+const p1 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject("p1");      
+        }, 50)
+        
+      
+    });
+  };
+
+const login = () => {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("a");      
+      }, 100)
+      
+    
+  });
+};
+
+const users = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("b");      
+      }, 200)
+  });
+};
+
+const apiCalls = async () => {
+    try {
+        // console.time('noraml promise')
+        // const r1 = await login();
+        // console.log(r1);
+        // const r2 = await users(r1);
+        // console.timeEnd('noraml promise')
+        // console.log(r2);
+        console.time('promise all')
+        const result = await Promise.allSettled([
+            p1(),
+            login(),
+            users()
+        ]);
+        console.log(result);
+        console.log(result.filter(x => x.status === 'fulfilled'));
+        console.timeEnd('promise all')
+    } catch (error) {
+        console.log(error);
+    }
+    finally {
+        console.log('finally call always');
+    }
+} 
+
+apiCalls()
+
+// old javascript technic
+// p1()
+//   .then((value) =>
+//     p2(value)
+//       .then((val) => console.log(val))
+//       .catch((err) => console.log(err))
+//   )
+//   .catch((err) => console.log("err", err));
